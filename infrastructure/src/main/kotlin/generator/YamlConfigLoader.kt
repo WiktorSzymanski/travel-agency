e4caa8dfd3fb268900061fcf8e34016bf58a45d3
Wiktor <darkvikos@gmail.com>
2025-06-30 15:22:41 +0200

    add: YamlConfigLoader with tests and EntityTemplates

---
+++ infrastructure/src/main/kotlin/generator/YamlConfigLoader.kt
@@ -0,0 +1,19 @@
+package generator
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import java.io.File
+
+class YamlConfigLoader {
+    val mapper = ObjectMapper(YAMLFactory()).apply {
+        registerKotlinModule()
+        registerModule(JavaTimeModule())
+    }
+
+    inline fun <reified T> loadConfig(filePath: String): List<T> {
+        return mapper.readValue(File(filePath))
+    }
+}
