df99e408b46aa0deb1e5eb2016139f3498ebfbb9
Wiktor <darkvikos@gmail.com>
2025-07-01 13:00:40 +0200

    add: Accommodation and AttractionGenerator with tests

--- infrastructure/src/main/kotlin/generator/AttractionGenerator.kt
+++ infrastructure/src/main/kotlin/generator/AttractionGenerator.kt
@@ -1,14 +1,33 @@
 package generator
 
 import loader.AttractionTemplate
+import loader.YamlConfigLoader
+import pl.szymanski.wiktor.ta.domain.LocationEnum
 import pl.szymanski.wiktor.ta.domain.aggregate.Attraction
+import java.time.Clock
+import java.time.LocalDateTime
+import java.util.UUID
 
-class AttractionGenerator : Generator<AttractionTemplate, Attraction> {
+class AttractionGenerator(
+    private val loader: YamlConfigLoader,
+    private val plusSeconds: Long,
+    private val clock: Clock = Clock.systemDefaultZone(),
+) : Generator<AttractionTemplate, Attraction> {
     override fun generate(filePath: String): List<Attraction> {
-        TODO("Not yet implemented")
+        val templates = loader.loadConfig<AttractionTemplate>(filePath)
+        return templates.map { toDomainModel(it) }
     }
 
-    override fun toDomainModel(template: AttractionTemplate): Attraction {
-        TODO("Not yet implemented")
-    }
+    override fun toDomainModel(template: AttractionTemplate): Attraction =
+        Attraction(
+            attractionId = UUID.randomUUID(),
+            name = template.name,
+            location = LocationEnum.valueOf(template.location.uppercase()),
+            date =
+                randomDateTimeBetween(
+                    LocalDateTime.now(clock),
+                    LocalDateTime.now(clock).plusSeconds(plusSeconds / 2),
+                ),
+            capacity = template.capacity,
+        )
 }
